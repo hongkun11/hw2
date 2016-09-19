9b368a66e26658791df6c3b310ad16127a2cea73
hongkun11 <hzhang5@cca.edu>
2016-09-19 10:17:26 -0700

    Create 3.js

---
+++ 3.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(200, 200);
+  background(255);
+
+  line(100,0,0,66);
+  line(100,0,200,66);
+  rectMode(CORNERS)
+  rect(0,66,199,199);
+  rectMode(CORNERS)
+  rect(120,134,160,200);
+}
