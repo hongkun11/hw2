722f1d839fe2b979ead1030dbd7791f45384a14f
hongkun11 <hzhang5@cca.edu>
2016-09-22 14:25:02 -0700

    Rename 7.js to 8.js

---
+++ 8.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(200, 200);
+  background(0);
+fill(255)
+  var i = 0;
+  while (i <20) {
+   ellipseMode(CENTER);
+ellipse(i*10,i*10,10,10);
+    i = i + 1;
+  }    
+}
