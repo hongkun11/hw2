6661be300688dfa750601e8214f5ce40ef720d02
hongkun11 <hzhang5@cca.edu>
2016-09-26 09:08:24 -0700

    Create homework1 no.3.js

---
+++ homework1 no.3.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400, 400);
+  background(255);
+
+for (a=20;a<390;a=a+20)
+  for (b= 20; b<390; b=b+20)
+  {
+          var g=random(255);
+var e=random(255)
+var f=random(255)
+stroke(255);
+fill(g,e,f);
+   ellipseMode(CENTER);
+ellipse(a,b,20,20);
+ }    
+}
