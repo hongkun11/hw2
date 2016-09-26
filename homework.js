b0ef0f268230919cf364e79898818515f8dc4f33
hongkun11 <hzhang5@cca.edu>
2016-09-26 08:58:24 -0700

    homework.js

---
+++ homework.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400, 400);
+  background(0);
+fill(255)
+  for (a=20;a<390;a=a+20)
+  for (b= 20; b<390; b=b+20)
+  {
+   ellipseMode(CENTER);
+ellipse(a,b,10,10);
+ }    
+}
